8811805b0c81b9aa6d9ea34d55c1d3b594c038d2
aditya-jha-material <141742732+aditya-jha-material@users.noreply.github.com>
2024-07-30 01:48:14 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
 
+// Route to handle form submission
+app.post('/api/form', (req, res) => {
+    const { monthlyGrossIncome, netIncome, housingCost, utilities, foodAndGroceries, transport, insurance, entertainment,
+        healthcare, education, savings, others, totalDebt, repaymentPlans, investment, pfFunds, property, emergencyFunds } = req.body;
+    // Process the form data here
+    console.log('Form data received:', { monthlyGrossIncome, netIncome, housingCost, utilities, foodAndGroceries, transport, insurance, entertainment,
+        healthcare, education, savings, others, totalDebt, repaymentPlans, investment, pfFunds, property, emergencyFunds });
+  
+    // Send a response back to the client
+    res.json({ success: true, message: 'Form data received successfully' });
+  });
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
