c5d518e62f8565a9e6f98ae7aaf384091781a559
= <=>
2024-07-31 23:14:04 +0530

    edited schema

--- server/models/Wallet.js
+++ server/models/Wallet.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
 const WalletSchema = new mongoose.Schema({
+    userName:{
+        type:String,
+        required:true,
+    },
     balance: {
         type: Number,
         required: true,
         default: 0
+    },
+    amount:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    Reason:{
+        type:String,
+        required:true
     },
     logs: [
         {
