ccb6f1efe2e39917e2fa1e393e8dd03279df0ddd
divyansh1010x <b23cm1015@iitj.ac.in>
2024-08-01 18:06:49 +0530

    added deposit withdraw button

--- client/src/pages/bottomNav.js
+++ client/src/pages/bottomNav.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import './bottomNav.css'; 
+import React, { useState } from 'react';
+import './bottomNav.css';
 
 const BottomNavbar = () => {
+  const [selectedOption, setSelectedOption] = useState('DEDUCT');
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <div className="bottom-navbar">
-      <input type="text" placeholder="Type something..." className="text-input" />
+      <input type="text" placeholder="Enter Amount" className="text-input" />
       <div className="button-container">
         <button className="btn">ADD</button>
-        <button className="btn">DEDUCT</button>
+        <div className="dropdown-btn">
+          <button className="btn action-btn">{selectedOption}</button>
+          <button className="btn dropdown-toggle">▼</button>
+          <div className="dropdown-content">
+            {/* add the href in the button */}
+            <button className="dropdown-item" onClick={() => handleOptionSelect('Entertainment')}>Entertainment</button>
+            <button className="dropdown-item" onClick={() => handleOptionSelect('Option 2')}>Utilities</button>
+            <button className="dropdown-item" onClick={() => handleOptionSelect('Option 3')}>Transport</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/wallet.js
+++ client/src/pages/wallet.js
@@ -3,17 +3,19 @@ import EditableFields from './editableFields';
 import ScrollBox from './scrollBox';
 import './wallet.css';
 import BottomNavbar from './bottomNav'; 
+import NavbarDemo from '../navbar-demo';
 
 const Wallet = () => {
   return (
     <div className="wallet-container">
-      <div className="editable-fields">
+      <div>
         <EditableFields />
       </div>
-      <div className="scroll-box-container">
+      <div className="scroll-box-container class">
+        <NavbarDemo />
         <ScrollBox />
+        <BottomNavbar /> {/* Add the BottomNavbar component */}
       </div>
-      <BottomNavbar /> {/* Add the BottomNavbar component */}
     </div>
   );
 };
