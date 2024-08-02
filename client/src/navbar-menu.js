ab52fc4f1339e5280aaf2900c5148fca59684506
Vandita1022 <b23cy1022@iitj.ac.in>
2024-08-02 15:32:59 +0530

    navbar changes

--- client/src/navbar-menu.js
+++ client/src/navbar-menu.js
@@ -58,27 +58,27 @@ export const Menu = ({ setActive, children }) => {
   );
 };
 
-export const ProductItem = ({ title, description, href, src }) => {
-  return (
-    <Link to={href} className="flex space-x-2">
-      <Img
-        src={src}
-        width={140}
-        height={70}
-        alt={title}
-        className="flex-shrink-0 rounded-md shadow-2xl"
-      />
-      <div>
-        <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
-          {title}
-        </h4>
-        <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
-          {description}
-        </p>
-      </div>
-    </Link>
-  );
-};
+// export const ProductItem = ({ title, description, href, src }) => {
+//   return (
+//     <Link to={href} className="flex space-x-2">
+//       <Img
+//         src={src}
+//         width={140}
+//         height={70}
+//         alt={title}
+//         className="flex-shrink-0 rounded-md shadow-2xl"
+//       />
+//       <div>
+//         <h4 className="text-xl font-bold mb-1 text-black dark:text-white">
+//           {title}
+//         </h4>
+//         <p className="text-neutral-700 text-sm max-w-[10rem] dark:text-neutral-300">
+//           {description}
+//         </p>
+//       </div>
+//     </Link>
+//   );
+// };
 
 export const HoveredLink = ({ children, ...rest }) => {
   return (
