1cb6bee602a7727be55e334e0231a5e51750d410
nayan-kute21 <nayankishorkute21@gmail.com>
2024-08-06 18:54:22 +0530

    fixed the context part in the backend

--- client/src/pages/Chat_App.js
+++ client/src/pages/Chat_App.js
@@ -6,53 +6,58 @@ import History from "./chatBot/History";
 import Clear from "./chatBot/Clear";
 import styles from "./Chat_App.module.css";
 import axios from "axios";
+
+var History_new;
+
 export default function Chat_App() {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [history, setHistory] = useState([]);
   let data;
-//   axios.get('http://localhost:12000/get_account',
-//     {params: {
-//         "userName" : sessionStorage.getItem("userName")
-//     }}
-//   )
-//   .catch((error)=>{
-//     console.log(error);
-//   })
-//   .then((response) =>{
-//      console.log(sessionStorage.getItem("userName"));
-//      data = response;
-//   })
-  
+  //   axios.get('http://localhost:12000/get_account',
+  //     {params: {
+  //         "userName" : sessionStorage.getItem("userName")
+  //     }}
+  //   )
+  //   .catch((error)=>{
+  //     console.log(error);
+  //   })
+  //   .then((response) =>{
+  //      console.log(sessionStorage.getItem("userName"));
+  //      data = response;
+  //   })
 
+  console.log(window.History_new);
   const handleSubmit = () => {
     const newMessage = { role: "user", content: input };
     let res = {};
-    
-    axios.post('http://localhost:12000/chatbot-', {
-        body: {
-            "context": {"role" : "user" , "message": "you are a financial advisor"}, 
-            "prompt": input
-        }
+
+    axios
+      .post("http://localhost:12000/chatbot-", {
+        context: [window.History_new],
+
+        prompt: input,
       })
       .then((response) => {
         console.log(response);
         res = { role: "assistant", content: response.data.response };
         setMessages([...messages, newMessage, res]);
 
+        const updatedHistory = [
+          ...history,
+          { question: input, answer: res.content },
+        ];
+        setHistory(updatedHistory);
+        window.History_new = updatedHistory;
       })
       .catch(function (error) {
         console.log(error);
       })
       .finally(function () {
         // always executed
-        setHistory([...history, { question: input, answer: res.content }]);
+        // setHistory([...history, { question: input, answer: res.content }]);
         setInput("");
       });
-
-
-
-
   };
 
   const clear = () => {
