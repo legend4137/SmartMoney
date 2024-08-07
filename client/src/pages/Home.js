935d66f91d67070eeef97c856d3858c970fab8d8
Danie <daniegeorgejohn@gmail.com>
2024-08-07 15:04:47 +0530

    Further Home page updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,6 @@ import Navbar from './navbar';
 import VantaBirds from './hero';
 import AboutUs from './aboutUs';
 import Footer from './footer';
-import styles from './Home.module.css';
 import aos from 'aos';
 import 'aos/dist/aos.css';
 import Features from './features';
@@ -15,7 +14,7 @@ function Home() {
   return (
     <div>
       <VantaBirds />
-    <div className={styles.container}>
+    <div >
       <div className="flex justify-center" data-aos="zoom-in" data-aos-duration="3000" >
             <div className='load'>
               <img className='w-64 max-w-xs pt-4 pb-8 z-200' src="/sample_logo.png" alt="Sample Logo" />
